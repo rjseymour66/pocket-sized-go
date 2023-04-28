7c7d8e9d262c0c00067d6f36ac4711d1a0e1568d
rseymour <rjs.github@gmail.com>
2023-04-28 00:08:58 -0400

    ready for testing

--- bookworm/bookworm.go
+++ bookworm/bookworm.go
@@ -1,8 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
+// A Bookworm contains the list of books on a bookworm's shelf.
 type Bookworm struct {
+	Name  string `json:"name"`
+	Books []Book `json:"books"`
+}
+
+// Book describes a book on a bookworm's shelf.
+type Book struct {
+	Author string `json:"author"`
+	Title  string `json:"title"`
 }
 
 func loadBookworms(filePath string) ([]Bookworm, error) {
-	return nil, nil
+	f, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	// Initialize the type the file decodes into.
+	var bookworms []Bookworm
+
+	// Decode the file and store the contents in the value bookworms.
+	// err = json.NewDecoder(f).Decode(&bookworms)
+	// if err != nil {
+	// 	return nil, err
+	// }
+	if err := json.NewDecoder(f).Decode(&bookworms); err != nil {
+		return nil, err
+	}
+
+	return bookworms, nil
 }
--- bookworm/main.go
+++ bookworm/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 
+	bookworms, err := loadBookworms("testdata/bookworms.json")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "failed to load bookworms: %s", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(bookworms)
 }
