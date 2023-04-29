7ae21fd6e62df7cac0ed77d4028d7b2a6bf6bc75
rseymour <rjs.github@gmail.com>
2023-04-29 00:12:19 -0400

    completed tests

---
+++ bookworm/bookworms_internal_test.go
@@ -0,0 +1,85 @@
+package main
+
+import "testing"
+
+var (
+	handmaidsTale = Book{Author: "Margaret Atwood", Title: "The Handmaid's Tale"}
+	oryxAndCrake  = Book{Author: "Margaret Atwood", Title: "Oryx and Crake"}
+	theBellJar    = Book{Author: "Sylvia Plath", Title: "The Bell Jar"}
+	janeEyre      = Book{Author: "Charlotte Brontë", Title: "Jane Eyre"}
+)
+
+func TestLoadBookworms(t *testing.T) {
+	type testCase struct {
+		bookwormsFile string
+		want          []Bookworm
+		wantErr       bool
+	}
+	tests := map[string]testCase{
+		"file exists": {
+			bookwormsFile: "testdata/bookworms.json",
+			want: []Bookworm{
+				{Name: "Fadi", Books: []Book{handmaidsTale, theBellJar}},
+				{Name: "Peggy", Books: []Book{oryxAndCrake, handmaidsTale, theBellJar}},
+			},
+			wantErr: false,
+		},
+		"file does not exist": {
+			bookwormsFile: "testdata/no_file_here.json",
+			want:          nil,
+			wantErr:       true,
+		},
+		"invalid JSON": {
+			bookwormsFile: "testdata/invalid.json",
+			want:          nil,
+			wantErr:       true,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			got, err := loadBookworms(tc.bookwormsFile)
+			if err != nil && !tc.wantErr {
+				t.Fatalf("expected an error %s, got none", err.Error())
+			}
+
+			if err == nil && tc.wantErr {
+				t.Fatalf("expected no error, got %s", err.Error())
+			}
+
+			if !equalBookworms(got, tc.want) {
+				t.Fatalf("different result: got %v, expected %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func equalBookworms(bookworms, target []Bookworm) bool {
+	if len(bookworms) != len(target) {
+		return false
+	}
+
+	for i := range bookworms {
+		if bookworms[i].Name != target[i].Name {
+			return false
+		}
+
+		if !equalBooks(bookworms[i].Books, target[i].Books) {
+			return false
+		}
+	}
+	return true
+}
+
+func equalBooks(books, target []Book) bool {
+	if len(books) != len(target) {
+		return false
+	}
+
+	for i := range books {
+		if books[i] != target[i] {
+			return false
+		}
+	}
+	return true
+}
